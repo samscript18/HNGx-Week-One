d9ad98828b6608ef2ee3b69898cecf948317e048
Adesugba Samuel <97664905+samscript18@users.noreply.github.com>
2023-09-09 07:46:23 -0700

    Update main.js

--- Week One/main.js
+++ Week One/main.js
@@ -8,18 +8,18 @@ const weekdays = [
   "Saturday",
 ];
 
-function getTime() {
+function updateTime() {
   const day = document.querySelector(".currentDay");
   const Time = document.querySelector(".currentTime");
 
   const date = new Date().getDay();
   const weekday = weekdays[date];
 
-  const time = new Date().getUTCMilliseconds();
+  const time = new Date().getTime();
 
   day.innerHTML = `${weekday}`;
   Time.innerHTML = `${time}`;
-  setTimeout(() => getTime(), 1000);
+  setTimeout(() => updateTime(), 1000);
 }
 
-getTime();
+updateTime();
